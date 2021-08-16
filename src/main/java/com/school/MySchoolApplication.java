bee3272b8744b00e5a5f9c18f2e4f9ae570f6b87
thirtsoft <thirtsoft@gmail.com>
2021-08-16 16:43:34 +0000

    first commit

---
+++ src/main/java/com/school/MySchoolApplication.java
@@ -0,0 +1,13 @@
+package com.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySchoolApplication.class, args);
+	}
+
+}
